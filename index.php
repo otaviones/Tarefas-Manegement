8823c2995d06a14eb5ba4fe8c024e8982fe08fed
otaviones <142029974+otaviones@users.noreply.github.com>
2025-03-27 17:01:26 -0300

    iniciacao
    Poject start!

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarefas</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="main-section">
+            <div class="add-section">
+                    <form action="">
+                        <input type="text" 
+                                name="title" 
+                                placeholder="Nova Tarefa">
+                        <button type="submit">Adicionar &nbsp; <span>&#43;</span></button>
+                    </form>
+            </div>
+            <div class="show-todo-section">
+                    <div class="todo-item">
+                        <input type="checkbox">
+                        <h2>Tarefa1</h2>
+                        <small>Criada: 27/03/2025</small>
+                    </div>
+            </div>
+    </div>
+</body>
+</html>
